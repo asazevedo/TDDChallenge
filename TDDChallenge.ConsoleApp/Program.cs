cb2d8b0bd667aa95128a480d7b0df74f40a45ea4
André Azevedo <andre.azevedo@imaginebeyond.com.br>
2021-08-24 15:02:52 -0300

    Added examples;

--- TDDChallenge.ConsoleApp/Program.cs
+++ TDDChallenge.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TDDChallenge.Services;
 
 namespace TDDChallenge.ConsoleApp
@@ -7,13 +8,55 @@ namespace TDDChallenge.ConsoleApp
     {
         static void Main(string[] args)
         {
+            ArrayOutlierExample();
+            ListFilterExample();
+            FormatWordsExample();
+
+            Console.ReadLine();
+        }
+
+        public static void ArrayOutlierExample()
+        {
             var arr = new int[] { 1, 3, 5, 53, 52, 55 };
             var service = new ArrayOutlierService(arr);
 
             var outlier = service.FindOutlier();
 
             Console.WriteLine($"The outlier is {outlier}");
-            Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void ListFilterExample()
+        {
+            var arr = new  List<object> { "abc", "test", "loren", 1, 2, 3 };
+            var service = new ListFilterService(arr);
+
+            var filter = service.FilterList(ListFilterService.FilterType.Strings);
+
+            Console.WriteLine($"List with only integers: ");
+            filter.ForEach(item =>
+            {
+                Console.WriteLine($"{item}");
+            });
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void FormatWordsExample()
+        {
+            var arr = new List<string> { "ninja", "samurai", "ronin" };
+            var service = new FormatWordsService(arr.ToArray());
+
+            var formatedWords = service.CommaSeparatedFormat();
+
+            Console.WriteLine($"Words with comma separated: {formatedWords}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
         }
     }
 }
